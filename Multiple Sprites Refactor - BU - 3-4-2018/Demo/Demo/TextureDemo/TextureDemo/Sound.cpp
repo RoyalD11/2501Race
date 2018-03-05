1e15924aad5370d495da63a63e4af16a2a4a5c41
Theofilos Plessas <theo.plessas@gmail.com>
2018-03-05 14:37:53 -0500

    Added sound class and sound file

---
+++ Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/Sound.cpp
@@ -0,0 +1,6 @@
+#include "Sound.h"
+
+void Sound::playersound() {
+	PlaySound(TEXT("Sounds/viper.wav"), NULL, SND_SYNC);
+
+}
---
+++ Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/Sound.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <Windows.h>
+#include <mmsystem.h>
+
+class Sound {
+public:
+	void playersound();
+
+private:
+
+
+};
