81af307360e9a58dc3c22ad365aa98ab44ba81cd
Theofilos Plessas <theo.plessas@gmail.com>
2018-03-05 15:24:10 -0500

    fixed the sound cutting into itself

--- Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/Sound.cpp
+++ Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/Sound.cpp
@@ -1,6 +1,6 @@
 #include "Sound.h"
 
 void Sound::playersound() {
-	PlaySound(TEXT("Sounds/viper.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	PlaySound(TEXT("Sounds/viper.wav"), NULL, SND_FILENAME |SND_NOSTOP| SND_ASYNC);
 
 }
