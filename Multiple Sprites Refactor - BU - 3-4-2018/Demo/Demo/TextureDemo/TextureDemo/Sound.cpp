7615ac5861c57d05e716e5929f1d611a88b91c03
Theofilos Plessas <theo.plessas@gmail.com>
2018-03-05 16:55:42 -0500

    Made it so  sound dosnt continue if car is stopped

--- Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/Sound.cpp
+++ Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/Sound.cpp
@@ -4,5 +4,8 @@ void Sound::playersound(float playerspeed) {
 	if(playerspeed > 0){
 		PlaySound(TEXT("Sounds/viper.wav"), NULL, SND_FILENAME | SND_NOSTOP | SND_ASYNC);
 	}
+	if (playerspeed == 0) {
+		PlaySound(NULL, NULL, 0);
+	}
 
 }
