c1fdc448e1e5504365dee5e8d9696b144bc61602
jerbyz <jer.crinson@gmail.com>
2018-03-23 15:09:20 -0400

    added staticrender

--- Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/GameEntity.cpp
+++ Multiple Sprites Refactor - BU - 3-4-2018/Demo/Demo/TextureDemo/TextureDemo/GameEntity.cpp
@@ -37,3 +37,36 @@ void GameEntity::render(Shader &shader, glm::vec3 cameraPos, float theta) {
 	glDrawElements(GL_TRIANGLES, numElements, GL_UNSIGNED_INT, 0);
 
 }
+
+void GameEntity::staticRender(Shader &shader) {
+	// Bind the entities texture
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	//Declaration of variables
+	glm::mat4 translationMatrix;
+	glm::mat4 scaleMatrix;
+	glm::mat4 rotationMatrix;
+	glm::mat4 transformationMatrix;
+
+	//translationMatrix = glm::translate(glm::mat4(1.0f), position - cameraPos);
+
+
+	//ISROT based transformations
+	//Set Scale
+	scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
+
+	//Set rotation
+	rotationMatrix = glm::rotate(glm::mat4(1.0f), rotationAmount, glm::vec3(0.0f, 0.0f, 1.0f));
+
+	//Set Translation
+	translationMatrix = glm::translate(glm::mat4(1.0f), position);
+
+	//Apply transformations
+	transformationMatrix = translationMatrix * rotationMatrix * scaleMatrix;
+	shader.setUniformMat4("x", transformationMatrix);
+
+
+	// Draw the entity
+	glDrawElements(GL_TRIANGLES, numElements, GL_UNSIGNED_INT, 0);
+
+}
